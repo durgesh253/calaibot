3917eeccb43a91b3cf409090a039ac1b89eb15e6
Durgesh Jadhav <jadhavdurgesh256@gmail.com>
2025-12-09 12:15:35 +0530

    new

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   server: {
     proxy: {
+      '/web-call': {
+        target: 'http://51.16.27.23:3001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/web-call/, '/web-call')
+      },
       '/api': {
         target: 'http://51.16.27.23:3001',
         changeOrigin: true,
